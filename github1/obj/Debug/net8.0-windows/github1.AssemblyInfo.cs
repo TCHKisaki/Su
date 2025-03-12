213b1135f165f7a6bb1d1f48335ba74a4d618a8c
DucVuong <9a4daoducvuong@gmail.com>
2025-03-12 10:53:20 +0700

    Second commit

--- github1/obj/Debug/net8.0-windows/github1.AssemblyInfo.cs
+++ github1/obj/Debug/net8.0-windows/github1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("github1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ad0409c7ecf0d8569058525d010ace1797744129")]
 [assembly: System.Reflection.AssemblyProductAttribute("github1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("github1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
